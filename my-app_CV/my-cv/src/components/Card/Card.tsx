a0a977de796d2e39a01ed3907698e73da9509fd5
Anastasia Gulenko <nansi0307@yahoo.com>
2022-12-27 17:32:24 +0200

    undo last changes

--- my-app_CV/my-cv/src/components/Card/Card.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Card: React.FC<{ text: string }> = ({ text }) => {
-  return (
-        <button onClick={() => { console.log(text) }}>{ text }</button>
-  )
-}
--- my-app_CV/my-cv/src/components/Summary/Summary.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { DATA } from '../../constants'
-import './Summary.scss'
-
-export const Summary: React.FC = () => {
-  return (
-        <section className='summary'>{DATA.summary}</section>
-  )
-}
